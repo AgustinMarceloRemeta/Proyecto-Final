9d6a8a0894043b726df30770564c020ffc8f33f2
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2025-02-19 16:29:53 -0300

    Update Dia.cs

--- Assets/CalendarAsset/_Scripts/Dia.cs
+++ Assets/CalendarAsset/_Scripts/Dia.cs
@@ -2,28 +2,35 @@
 using TMPro;
 using UnityEngine.UI;
 
+public enum EstadoAsistencia
+{
+    Asistio,    // Verde
+    NoAsistio,  // Rojo
+    SinClase    // Gris
+}
+
 public class Dia : MonoBehaviour
 {
     private TextMeshProUGUI _diaTexto;
+    private Button button;
+    public bool selected = false;
+    public bool weekend = false;
+    private EstadoAsistencia estadoActual = EstadoAsistencia.SinClase;
 
-    Button button;
-    bool selected= false;
-    bool attended= false;
-    public bool weekend;
     private void Awake()
     {
         _diaTexto = gameObject.GetComponentInChildren<TextMeshProUGUI>();
         button = GetComponentInChildren<Button>();
     }
 
-    private void Start()
-    {
-    }
-
     public void SetButton()
     {
-        if (weekend) _diaTexto.color = Color.grey;
-        else button.onClick.AddListener(() => OnClickButton());
+        if (weekend)
+        {
+            estadoActual = EstadoAsistencia.SinClase;
+            ActualizarColor();
+        }
+        button.onClick.AddListener(() => OnClickButton());
     }
 
     public void SetDiaAtivo(bool ativo)
@@ -38,24 +45,56 @@ public class Dia : MonoBehaviour
 
     public void OnClickButton()
     {
+        // Agregar debug para ver el flujo
+        Debug.Log($"Click - Estado Actual: {estadoActual}, Selected: {selected}");
+
         if (!selected)
         {
-            _diaTexto.color = Color.green;
             selected = true;
-            attended = true;
+            estadoActual = EstadoAsistencia.Asistio;
+            ActualizarColor();
         }
         else
         {
-            if (attended)
-            {
+            // Si está seleccionado, cambia al siguiente estado
+            CambiarEstado();
+        }
+
+        Debug.Log($"Después del cambio - Estado Actual: {estadoActual}, Selected: {selected}");
+    }
+
+    private void CambiarEstado()
+    {
+        switch (estadoActual)
+        {
+            case EstadoAsistencia.Asistio:
+                estadoActual = EstadoAsistencia.NoAsistio;
+                break;
+            case EstadoAsistencia.NoAsistio:
+                estadoActual = EstadoAsistencia.SinClase;
+                break;
+            case EstadoAsistencia.SinClase:
+                estadoActual = EstadoAsistencia.Asistio;
+                break;
+        }
+
+        ActualizarColor();
+        // No deseleccionamos aquí para permitir múltiples cambios
+    }
+
+    private void ActualizarColor()
+    {
+        switch (estadoActual)
+        {
+            case EstadoAsistencia.Asistio:
+                _diaTexto.color = Color.green;
+                break;
+            case EstadoAsistencia.NoAsistio:
                 _diaTexto.color = Color.red;
-                attended = false;
-            }
-            else
-            {
-                _diaTexto.color= Color.green;
-                attended = true;
-            }
+                break;
+            case EstadoAsistencia.SinClase:
+                _diaTexto.color = Color.grey;
+                break;
         }
     }
 }
