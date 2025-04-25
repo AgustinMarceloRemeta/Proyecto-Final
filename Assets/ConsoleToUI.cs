6f92cc2aa4dfcf1e6ad367644a808ae16f6aee97
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2025-04-24 22:28:45 -0300

    Fix firebase

---
+++ Assets/ConsoleToUI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+
+public class ConsoleToUI : MonoBehaviour
+{
+    public TextMeshProUGUI logText; // Arrastrá un Text UI acá desde el Canvas
+    private string currentLog = "";
+    private Queue<string> logQueue = new Queue<string>();
+    public int maxLogs = 30;
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        string log = $"[{type}] {logString}";
+        logQueue.Enqueue(log);
+
+        if (logQueue.Count > maxLogs)
+            logQueue.Dequeue();
+
+        currentLog = string.Join("\n", logQueue.ToArray());
+        logText.text = currentLog;
+    }
+}
